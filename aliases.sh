71ebc3a87021add8c37e27110166e9bfb3b88cd2
Basheer Subei <6508762+bsubei@users.noreply.github.com>
2025-01-19 16:39:34 +0000

    add a couple of bash aliases to make running maelstrom easier

---
+++ aliases.sh
@@ -0,0 +1,2 @@
+alias mael-test="cargo build && ./maelstrom/maelstrom test -w echo --bin ./target/debug/maelstrom-tutorial --nodes n1 --time-limit 10 --log-stderr"
+alias mael-serve="./maelstrom/maelstrom serve"
