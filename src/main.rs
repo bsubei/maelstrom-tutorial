0e423c0848cc5d175884f36dfac56ee93ea5febb
Basheer Subei <6508762+bsubei@users.noreply.github.com>
2025-01-19 14:43:15 +0000

    basic Message type using serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+use std::io::BufRead;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum MessageBody {
+    Init {
+        msg_id: Option<usize>,
+        in_reply_to: Option<usize>,
+        node_id: String,
+        node_ids: Vec<String>,
+    },
+    InitOk {
+        msg_id: Option<usize>,
+        in_reply_to: Option<usize>,
+    },
+}
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    src: String,
+    dest: String,
+    body: MessageBody,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        if let Ok(line) = line {
+            let msg: Message = serde_json::from_str(&line).unwrap();
+            eprintln!("Received message: {}", serde_json::to_string(&msg).unwrap());
+        }
+    }
 }
