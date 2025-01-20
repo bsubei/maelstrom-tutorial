4d2f29de79d3478e8269e3a98c008b9b79a90ef8
Basheer Subei <6508762+bsubei@users.noreply.github.com>
2025-01-20 16:13:09 +0000

    spawn a new thread for each incoming message we are handling

--- src/main.rs
+++ src/main.rs
@@ -5,45 +5,53 @@ use node::{send_echo_reply, send_init_reply, send_topology_reply, Node};
 use protocol::{Message, MessageBody};
 use std::default::Default;
 use std::io;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     let stdin = io::stdin();
 
-    let mut node: Node = Default::default();
+    let node = Arc::new(Mutex::<Node>::new(Default::default()));
 
     for line in stdin.lines() {
         if let Ok(line) = line {
             let msg: Message = serde_json::from_str(&line).unwrap();
-            match &msg.body {
-                MessageBody::Init {
-                    node_id, node_ids, ..
-                } => {
-                    node.log(&format!("Got Init message: {msg:?}"));
 
-                    // Initialize our node.
-                    node.id = node_id.clone();
-                    node.ids = node_ids.clone();
+            let node_ref = node.clone();
+            std::thread::spawn(move || {
+                match &msg.body {
+                    MessageBody::Init {
+                        node_id, node_ids, ..
+                    } => {
+                        let mut node = node_ref.lock().unwrap();
+                        node.log(&format!("Got Init message: {msg:?}"));
 
-                    // Reply back with an init_ok.
-                    send_init_reply(&mut node, msg);
-                }
-                MessageBody::Echo { .. } => {
-                    node.log(&format!("Got Echo message: {msg:?}"));
+                        // Initialize our node.
+                        node.id = node_id.clone();
+                        node.ids = node_ids.clone();
 
-                    send_echo_reply(&mut node, msg);
-                }
-                MessageBody::Topology { topology, .. } => {
-                    node.log(&format!("Got topology message: {msg:?}"));
-
-                    if let Some(neighbors) = topology.get(&node.id) {
-                        node.neighbor_ids = Some(neighbors.clone());
-                        node.log(&format!("Setting neighbors to {:?}", neighbors));
+                        // Reply back with an init_ok.
+                        send_init_reply(&mut node, msg);
                     }
+                    MessageBody::Echo { .. } => {
+                        let mut node = node_ref.lock().unwrap();
+                        node.log(&format!("Got Echo message: {msg:?}"));
 
-                    send_topology_reply(&mut node, msg);
-                }
-                _ => todo!(),
-            };
+                        send_echo_reply(&mut node, msg);
+                    }
+                    MessageBody::Topology { topology, .. } => {
+                        let mut node = node_ref.lock().unwrap();
+                        node.log(&format!("Got topology message: {msg:?}"));
+
+                        if let Some(neighbors) = topology.get(&node.id) {
+                            node.neighbor_ids = Some(neighbors.clone());
+                            node.log(&format!("Setting neighbors to {:?}", neighbors));
+                        }
+
+                        send_topology_reply(&mut node, msg);
+                    }
+                    _ => todo!(),
+                };
+            });
         }
     }
 }
